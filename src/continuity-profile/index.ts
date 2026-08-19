ee8a464547b1deec8ff6d51036044c0f812da0d0
the static collective <joyvolcanomusic@gmail.com>
2026-08-19 16:38:38 -0500

    feat: define typed continuity braid v0 contract

---
+++ src/continuity-profile/index.ts
@@ -0,0 +1,13 @@
+export {
+  CONTINUITY_LANES,
+  CONTINUITY_MODES,
+} from "./types";
+
+export type {
+  ContinuityClaimV0,
+  ContinuityDimension,
+  ContinuityEnvironment,
+  ContinuityLaneClaim,
+  ContinuityLaneKind,
+  ContinuityMode,
+} from "./types";
